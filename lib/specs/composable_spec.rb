9f4ff7cf8229196ecd645c0fd8423bc63423262a
Robert Citek <robert.citek@gmail.com>
2015-04-30 13:51:52 -0400

    adjusted indentation

--- lib/specs/composable_spec.rb
+++ lib/specs/composable_spec.rb
@@ -12,13 +12,11 @@ describe Composable do
   end
   describe 'compose' do
     it 'should return sudo for to_s' do
-    @sudo.to_s.must_equal 'sudo'
-  end
-
-  it 'should have .prev == nil' do
-    @sudo.prev.must_equal nil
+      @sudo.to_s.must_equal 'sudo'
     end
-
+    it 'should have .prev == nil' do
+      @sudo.prev.must_equal nil
+    end
     it 'should return sudo for compose' do
       @sudo.compose.must_equal 'sudo'
     end
@@ -35,10 +33,9 @@ describe 'compose two objects' do
   it 'should be instance of Docker' do
     @docker.class.name.must_equal 'Docker'
   end
-   it 'should return docker for Docker.to_s' do
+  it 'should return docker for Docker.to_s' do
     @docker.to_s.must_equal 'docker'
   end
-
   it 'should return "sudo docker"' do
     @docker.compose.must_equal 'sudo docker'
   end
